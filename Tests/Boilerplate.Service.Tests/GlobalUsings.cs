0c7afb92ce503118998224b020a3b87b3e9c1006
Cagatay Karaahmetoglu <ckaraahm@lely.com>
2022-10-03 20:59:26 +0200

    Cleans unused imports

--- Tests/Boilerplate.Service.Tests/GlobalUsings.cs
+++ Tests/Boilerplate.Service.Tests/GlobalUsings.cs
@@ -2,11 +2,7 @@
 
 global using System;
 global using System.Collections.Generic;
-global using System.Net;
-global using System.Net.Http;
-global using System.Threading;
 global using System.Threading.Tasks;
 global using AutoMapper;
 global using Moq;
-global using Moq.Protected;
 global using Xunit;
